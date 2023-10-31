e645ae3f04b59712385bc86bb7502d974abeb6b8
Julia <julia.r.lindgren@gmail.com>
2023-10-31 14:09:41 +0000

    Set up project files and install webpack and eslint

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
